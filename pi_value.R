43c8f0f8560d6048b533f839bae1f7d2dfa6563b
pankajmishra000 <36365254+pankajmishra000@users.noreply.github.com>
2018-02-18 10:47:04 +0100

    Pi Value approximation in R
    Codes for approximating the values of Pi in R.
    I used 
    1)Trapezoidal method
    2) Mid point Method and
    3)Simpson Method
    to approximate the value of pi.
    Everyone knows that the value of pi = 22/7 but the actual definition of pi is below-
    Pi = Area of circle/ radius^2
    
    I used this definition to approximate the values of pi using numerical integration methods.

---
+++ pi_value.R
@@ -0,0 +1,63 @@
+##### Pi value
+##Trapezoidal application
+
+y = function(x){
+  
+  return(sqrt(1-x^2))
+}
+
+area = function(b=1){
+  
+  h=0.0001
+  x =0
+  x = x+h
+  Ar =0
+  while(b>x){
+    Ar = Ar + 0.5*(y(x)+y(x-h))*h
+    x = x+h
+    
+  }
+  
+  return(Ar)
+  
+}
+
+#######Mid point rule######
+area_midpt = function(b=1){
+  
+  h=0.0001
+  x =0
+  x = x+h
+  Ar =0
+  while(b>x){
+    Ar = Ar + (y((x + x -h)/2))*h
+    x = x+h
+    
+  }
+  
+  return(Ar)
+  
+}
+
+#####Simpson Rule######
+
+area_simpson = function(b=1){
+  
+  h=0.0001
+  x =0
+  x = x+h
+  Ar =0
+  while(b>x){
+    Ar = Ar + (y(x-h)+ 4*(y((x+x-h)/2))+y(x))*h/6
+    x = x+h
+    
+  }
+  
+  return(Ar)
+  
+}
+
+
+
+
+
